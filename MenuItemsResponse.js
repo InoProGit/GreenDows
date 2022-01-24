4e4627a90e8289d242acd23d82f0eeaf4accc4c1
InoProGit <ignat.georgievich@gmail.com>
2022-01-24 15:54:02 +0500

    happy new year!

---
+++ MenuItemsResponse.js
@@ -0,0 +1,20 @@
+const jsonMenuItemsResponse = `{
+    "tasks" : [
+        {
+            "id" : "1",
+            "name" : "Task"
+        },
+        {
+            "id" : "2",
+            "name" : "Default Radar"
+        },
+        {
+            "id" : "3",
+            "name" : "Task solution"
+        },
+        {
+            "id" : "4",
+            "name" : "Interface Solution"
+        }
+    ]
+}`;
---
+++ windowsResponse.js
@@ -0,0 +1,24 @@
+const windowsResponse = `{
+    tasks : [
+      {
+        "id": 1,
+        "name": "Task",
+        "windowData": ""
+      },
+      {
+        "id": 2,
+        "name": "Default Radar",
+        "windowData": ""
+      },
+      {
+        "id": 3,
+        "name": "Task solution",
+        "windowData": ""
+      },
+      {
+        "id": 4,
+        "name": "Interface Solution",
+        "windowData": ""
+      }
+    ]
+}`;
